747e1af80f85e52896f42dc0b389bd58da986331
Cakeapo <41561811+Cakeapo@users.noreply.github.com>
2018-12-03 19:02:37 +1300

    Basic movement
    Very simple movement/jumping and double jumping

---
+++ Unity Projects/CottonKingdom/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float runSpeed, walkSpeed, jumpForce;
+    private float moveSpeed;
+    public bool isGrounded, isRunning;
+
+    public float velX, velY, velZ;
+    public int jumpCount;
+
+    public Rigidbody rb;
+    public LayerMask groundedMask;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.eulerAngles = new Vector3(0 + rb.velocity.z, 0, 0 - rb.velocity.x);
+
+        //Check if player is Grounded        
+        isGrounded = Physics.CheckSphere(transform.position, 0.3f, groundedMask);
+
+        //Check if walk input is pressed
+        isRunning = !Input.GetButton("Walk");
+
+        //Set sprint and walk speed
+        if (isRunning)
+            moveSpeed = runSpeed;
+        else
+            moveSpeed = walkSpeed;
+
+        //basic movement
+        rb.velocity = new Vector3(velX, rb.velocity.y, velZ);
+
+        velX = Input.GetAxis("Horizontal") * moveSpeed;
+        velZ = Input.GetAxis("Vertical") * moveSpeed;
+
+        //jumpcounter for double jumping
+        if (isGrounded)
+            jumpCount = 1;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (jumpCount > 0)
+            {
+                rb.velocity += Vector3.up * jumpForce;
+                jumpCount--;
+            }
+        }
+    }
+}
