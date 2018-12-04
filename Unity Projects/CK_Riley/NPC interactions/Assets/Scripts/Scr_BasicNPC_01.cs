2db035694a494f7ac26228db00f2da655ff3a79b
Ripster <Ripster27@gmail.com>
2018-12-04 18:25:44 +1300

    Adding Riley's Unity files.
    Uploading my unity project files for NPC testing.

---
+++ Unity Projects/CK_Riley/NPC interactions/Assets/Scripts/Scr_BasicNPC_01.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_BasicNPC_01 : MonoBehaviour {
+
+    public bool hasInteracted = false, isInteractable = false, hasInformation = true;
+
+    public int interactionTotal = 0;
+    public int newInteractionsTotal;
+    [Space(5)]
+    public string dataName, playerTag;
+
+    private Scr_DataSaver_01 dataSaver;
+    [Space(5)]
+    public GameObject info;
+
+	// Use this for initialization
+	void Start ()
+    {
+        LoadData();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (hasInformation == true)
+        {
+            info.SetActive(true);
+            //&& isInteractable == true
+        }
+        else info.SetActive(false);
+
+        if (isInteractable == true)
+        {
+            //print("Hi.");
+            print("NPC Interaction_01");
+        }
+
+    }
+
+    private void LoadData()
+    {
+        dataSaver = GetComponent<Scr_DataSaver_01>();
+        print("Retrive interactions >>>");
+        interactionTotal = dataSaver.DataGetInt(dataName);
+        print("Set Interactions count >>>");
+        if (interactionTotal > 0)
+        {
+            hasInteracted = false;
+            print("First Interaction enabled >>>");
+        }
+        print("Loaded " + dataName);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        print("Triggered.");
+        if (other.transform.tag == playerTag)
+        {
+            isInteractable = true;
+            dataSaver.DataSaveInt(dataName, interactionTotal += 1);
+            print("Interactin with " + dataName + ", "+ dataSaver.DataGetInt(dataName));
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == playerTag)
+        {
+            isInteractable = false;
+            print("Interaction finished with " + dataName + ", " + dataSaver.DataGetInt(dataName));
+        }
+    }
+}
---
+++ Unity Projects/CK_Riley/NPC interactions/Assets/Scripts/Scr_DataSaver_02.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_DataSaver_02 : MonoBehaviour {
+
+
+    public int returnInt;
+	// Use this for initialization
+
+    public void DataSaveInt(string dataName,  int dataValueInt)
+    {
+        PlayerPrefs.SetInt(dataName, dataValueInt);
+    }
+
+    public int DataGetInt(string dataName)
+    {
+        //returnInt = PlayerPrefs.GetInt(dataName, 0);
+        //ReturnInt();
+        //return returnInt;
+        if (PlayerPrefs.HasKey(dataName))
+        {
+            return PlayerPrefs.GetInt(dataName, 0);
+        }
+        else
+        {
+            PlayerPrefs.SetInt(dataName, 0);
+            return 0;
+        }
+    }
+    /*
+    public int ReturnInt()
+    {
+        return returnInt;
+    }*/
+}
