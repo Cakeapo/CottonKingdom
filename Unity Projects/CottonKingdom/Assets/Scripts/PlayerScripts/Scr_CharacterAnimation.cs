62ff057e2bb9c7d2638ba8026d05c5888349955b
Cakeapo <41561811+Cakeapo@users.noreply.github.com>
2018-12-05 18:57:46 +1300

    Animations
    Basic animation tree

---
+++ Unity Projects/CottonKingdom/Assets/Scripts/PlayerScripts/Scr_CharacterAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_CharacterAnimation : MonoBehaviour
+{
+    public Animator anim;
+    public Scr_PlayerMovement playerMovement;
+
+    public float moveForward, moveStrafe;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerMovement = GetComponent<Scr_PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        moveForward = playerMovement.input.z;
+        moveStrafe = playerMovement.input.x;
+
+        if (playerMovement.isRunning)
+        {
+            anim.SetBool("Walking", false);
+        }
+        else
+        {
+            anim.SetBool("Walking", true);
+        }
+
+        if(moveForward == 0 && moveStrafe == 0)
+        {
+            anim.SetBool("Moving", false);
+        }
+        else
+        {
+            anim.SetBool("Moving", true);
+        }
+
+        anim.SetFloat("SpeedForward", moveForward);
+        anim.SetFloat("SpeedStrafe", moveStrafe);
+    }
+
+    public void Jump()
+    {
+        anim.SetTrigger("Jump");
+    }
+}
