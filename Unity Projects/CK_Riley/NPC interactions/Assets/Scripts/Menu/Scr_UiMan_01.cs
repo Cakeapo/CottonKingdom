08d23315b39c9147c85590c595201efe0fb823fd
Ripster7 <34284620+Ripster7@users.noreply.github.com>
2019-02-11 18:59:49 +1300

    Updates to UI.
    Inventory UI popup implemented and started for dropdown UI (Main menu).
    
    //Player ctrls need to be deactivated when in menu.

--- Unity Projects/CK_Riley/NPC interactions/Assets/Scripts/Menu/Scr_UiMan_01.cs
+++ Unity Projects/CK_Riley/NPC interactions/Assets/Scripts/Menu/Scr_UiMan_01.cs
@@ -12,7 +12,9 @@ public class Scr_UiMan_01 : MonoBehaviour
     // image to the item that was collected.
 
     public Canvas inventoryCanvasUI;
-    public bool testing;
+    public Animator invUIAnim0, invUIAnim1;
+    bool invUIbool = false;
+    public bool testing, inMenu;
 
     void Start()
     {
@@ -20,12 +22,27 @@ public class Scr_UiMan_01 : MonoBehaviour
         {
             inventoryCanvasUI.enabled = false;
         }
-        
+        invUIAnim0 = inventoryCanvasUI.transform.GetChild(0).gameObject.GetComponent<Animator>();
+        invUIAnim1 = inventoryCanvasUI.transform.GetChild(1).gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape) == true)
+        {
+            InventoryAnim();
+        }
+    }
+
+    void InventoryAnim()
+    {
+        invUIbool = !invUIbool;
+        inMenu = invUIbool; //works for temporary, once there are several menues it needs to check
+                            // if there is another menu activated
+        invUIAnim1.SetBool("IsUp", invUIbool);
+        invUIAnim0.SetBool("IsUp", invUIbool);
+
+
     }
 }
