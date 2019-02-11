9fcb00c11042ca0416c4a86af6cf81f0bd9f8025
Ripster7 <34284620+Ripster7@users.noreply.github.com>
2019-02-11 18:20:02 +1300

    Updates to inventory and UI.
    Added the ability for timed respawns and self deleting collectables.
    
    Started to implement a UI Manager to assist with UI calls.

--- Unity Projects/CK_Riley/NPC interactions/Assets/Scripts/Items/Scr_AddToInv_01.cs
+++ Unity Projects/CK_Riley/NPC interactions/Assets/Scripts/Items/Scr_AddToInv_01.cs
@@ -5,22 +5,70 @@ using UnityEngine;
 public class Scr_AddToInv_01 : MonoBehaviour {
 
     //Obj name to referance what gets added to inventory
-    public string ObjName;
-    public bool isCollected, toDelet;
+    public string objName;
+    public bool selfAssignName;
+    public bool isCollected, toDelet, isRespawn, isDeleted;
+
+    public GameObject objectToSpawn;
+
+    public float respawnTimer, respawnAt;
 
 	// Use this for initialization
 	void Start () {
 
-        if (ObjName == "")
+
+
+        isDeleted = false;
+
+        respawnTimer = 0;
+
+        if (isRespawn == true && objectToSpawn == null)
         {
-            Debug.LogError("memberVariable must be set to the corrasponding PlayerPrefs counter name for "
-            + gameObject.name + ".");
+                Debug.LogError("memberVariable must be assigned. GameObject to respawn is empty for "
+                + gameObject.name + ".");
+
+        }
+
+        if (selfAssignName == true)
+        {
+            objName = gameObject.name;
+        }
+        else
+        {
+            if (objName == "")
+            {
+                Debug.LogError("memberVariable must be set to the corrasponding PlayerPrefs counter _Name_ for "
+                + gameObject.name + ".");
+            }
         }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(isRespawn == true)
+        {
+            if(respawnTimer < respawnAt && isDeleted == true)
+            {
+                respawnTimer += Time.deltaTime;
+            }
+            else
+            {
+                if (isDeleted == true && toDelet == true)
+                {
+                    isDeleted = false;
+                    GameObject objName = (GameObject)Instantiate(objectToSpawn, 
+                        transform.position, transform.rotation);
+                    objName.transform.parent = gameObject.transform;
+                    toDelet = false;
+                    respawnTimer = 0;
+
+                }
+            }
+
+        }
+
+
 	}
 
     public void OnTriggerEnter(Collider other)
@@ -35,10 +83,11 @@ public class Scr_AddToInv_01 : MonoBehaviour {
 
 
             //add to inventory
-            int tempInt = PlayerPrefs.GetInt(ObjName);
-            if(ObjName != "")   
+            int tempInt = PlayerPrefs.GetInt(objName);
+            if(objName != "")   
             {
-                PlayerPrefs.SetInt(ObjName, (tempInt + 1));
+                PlayerPrefs.SetInt(objName, (tempInt + 1));
+                toDelet = true;
             }
                 /*If the Object is not named to the correct object that is assigned to a 
                 PlayerPres account then it will return an error. 
@@ -49,7 +98,7 @@ public class Scr_AddToInv_01 : MonoBehaviour {
                 Debug.LogError("memberVariable must be set to the corrasponding PlayerPrefs counter name for "
                     + gameObject.name + ".");
                 Debug.LogError("Autoassigned memberVarible name to: '" + gameObject.name + "'.");
-                ObjName = gameObject.name;
+                objName = gameObject.name;
             }
             
             
@@ -57,7 +106,14 @@ public class Scr_AddToInv_01 : MonoBehaviour {
             //delet self
             if(toDelet == true)
             {
-                Destroy(gameObject);
+                if (isRespawn == true)
+                {
+                    Destroy(gameObject.transform.GetChild(0).gameObject); //Delets the child the respawns after x seconds
+                    
+                    isDeleted = true;
+                }
+
+                else Destroy(gameObject);
             }
 
         }
---
+++ Unity Projects/CK_Riley/NPC interactions/Assets/Scripts/Menu/Scr_UiMan_01.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_UiMan_01 : MonoBehaviour
+{
+    //self tag = "UI_Manager"
+
+    //This manager is used to show what objects are collectd and when to show the appropriate UI's.
+    //Such as having the popup UI show and having the collection UI popup show the corrosponding 
+    // image to the item that was collected.
+
+    public Canvas inventoryCanvasUI;
+    public bool testing;
+
+    void Start()
+    {
+        if(testing == true)
+        {
+            inventoryCanvasUI.enabled = false;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
